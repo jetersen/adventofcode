2585fbe56c398e3b2a262a387f892a19ac39a7a0
Joseph Petersen <josephp90@gmail.com>
2021-12-02 07:36:29 +0100

    add day 2

---
+++ src/AdventOfCode.Year2021/Day02.cs
@@ -0,0 +1,68 @@
+﻿namespace AdventOfCode.Year2021;
+
+public sealed class Day02 : BaseDay
+{
+    private string[] _input = Array.Empty<string>();
+
+    public Day02(IAdventClient client, IEnvironment environment, IFileSystem fileSystem) : base(client, environment, fileSystem)
+    {
+    }
+
+    public override async Task LoadInput() => _input = (await File.ReadAllLinesAsync(InputFilePath.FullPath));
+
+    public override ValueTask<string> Solve_1()
+    {
+        var position = 0;
+        var depth = 0;
+
+        foreach (var instruction in _input)
+        {
+            var direction = instruction[0];
+            var distance = int.Parse(instruction[^1..]);
+
+            switch (direction)
+            {
+                case 'f':
+                    position += distance;
+                    break;
+                case 'd':
+                    depth += distance;
+                    break;
+                case 'u':
+                    depth -= distance;
+                    break;
+            }
+        }
+
+        return new((position * depth).ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var position = 0;
+        var depth = 0;
+        var aim = 0;
+
+        foreach (var instruction in _input)
+        {
+            var direction = instruction[0];
+            var distance = int.Parse(instruction[^1..]);
+
+            switch (direction)
+            {
+                case 'f':
+                    position += distance;
+                    depth += aim * distance;
+                    break;
+                case 'd':
+                    aim += distance;
+                    break;
+                case 'u':
+                    aim -= distance;
+                    break;
+            }
+        }
+
+        return new((position * depth).ToString());
+    }
+}
